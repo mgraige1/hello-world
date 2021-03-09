adfc243c4798d5b6749dab9520ec54e6d51b45d1
Matthew <mgraige1@email.cpcc.edu>
2021-03-09 11:34:21 -0500

    Revert "Update Helloworld.java"
    This reverts commit 62aad770aada77324140f87fcb5ad3965d93d749.

--- Helloworld.java
+++ Helloworld.java
@@ -3,14 +3,5 @@ public class Helloworld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
-      
-      String name = "George Washinton";
-      
-      System.out.println(name);
-      
-      int age =24;
-      
-      System.out.println(age);
-      
    }
 }
