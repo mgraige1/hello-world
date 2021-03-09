62aad770aada77324140f87fcb5ad3965d93d749
Matthew <mgraige1@email.cpcc.edu>
2021-03-09 11:32:37 -0500

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,14 @@ public class Helloworld
    public static void main(String[] args)
    {
       System.out.println("Hello World!");
+      
+      String name = "George Washinton";
+      
+      System.out.println(name);
+      
+      int age =24;
+      
+      System.out.println(age);
+      
    }
 }
